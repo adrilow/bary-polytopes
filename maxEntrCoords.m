aeaa81f72c5d23179af28ebdbb284468ce16bb8a
drilow <drilow@hotmail.com>
2017-06-24 13:48:25 +0200

    Initial (untested) prototype for maxEntrCoords using constant prior functions. Simple tests are also set up.

--- maxEntrCoords.m
+++ maxEntrCoords.m
@@ -1,8 +1,11 @@
 function [ b ] = maxEntrCoords( omega, v )
 % maxEntrCoords Calculates maximum entropy coordinates for a given Polytope
-%   
-% Uses Algorithm by Hormann & Sukumar (2008)
-% 
+%
+% Uses Algorithm by Hormann & Sukumar (2008), currently with constant
+% priors, so it ressembles the coordinates by Arroyo & Ortiz (2006), which
+% satisfy partition of unity and linear precission, but the lagrange
+% constraint only for strictly convex polytopes.
+%
 % Inputs:
 % omega : [v1, ..., vn] (vi [w1, ..., wd] d : dim of omega) vertices of
 % the polytope omega.
@@ -10,6 +13,62 @@ function [ b ] = maxEntrCoords( omega, v )
 %
 % Outputs:
 % b : [b1v, ..., bnv] barycentric coordinates for v in omega.
+
+b = zeros(length(omega),length(omega(1)));
+
+
+
+%1)
+vtilde = arrayfun(@(vi) vi-v, omega);
+m = ones(length(omega)); % Using constant priors, this has to be updated
+
+%2)
+k = 0;
+lambda = 0;
+epsilon = 10e-10;
+
+while True
+    %3)
+    f,g,H = brownfgh(lambda);
+    
+    %4)
+    deltalambda = -(1/H) * g;
+    
+    %5)
+    alpha = 1; %for the moment
+    lambda = lambda + (alpha * deltalambda);
+    
+    %6)
+    if (norm(gradient(F(lambda))) <= epsilon)
+        break;
+    else
+        k = k+1
+    end
+end
+
+%7)
+for i = 1:length(omega)
+    b(i) = Zi(lambda,i) / Z(lambda);
+end
+
+% Helper functions
+
+    % Z is the partition function
+    function [zi] = Zi(lambda,j)
+        zi = m(i)*exp((-lambda)*vtilde(j));
+    end
+
+    function [z] = Z(lambda)
+        z = 0;
+        for j = 1:length(omega)
+            z = z + zi(lambda,j);
+        end
+    end
+
+    % F 
+    function [f] = F(lambda)
+        f = ln(Z(lambda));
+    end
 
 
 end
---
+++ tbxmanager/toolboxes/yalmip/R20170624/all/YALMIP-master/Contents.m
@@ -0,0 +1,3 @@
+% YALMIP
+% Version 22-Jun-2017
+% Help on http://yalmip.github.io
