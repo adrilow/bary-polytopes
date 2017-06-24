6d5103b747274297460a4b6b75be74ee0b2c6f08
drilow <drilow@hotmail.com>
2017-06-24 11:02:49 +0200

    Setup of maxEntrCoords function.

---
+++ maxEntrCoords.m
@@ -0,0 +1,16 @@
+function [ b ] = maxEntrCoords( omega, v )
+% maxEntrCoords Calculates maximum entropy coordinates for a given Polytope
+%   
+% Uses Algorithm by Hormann & Sukumar (2008)
+% 
+% Inputs:
+% omega : [v1, ..., vn] (vi [w1, ..., wd] d : dim of omega) vertices of
+% the polytope omega.
+% v : [a1, ..., ad] point in omega.
+%
+% Outputs:
+% b : [b1v, ..., bnv] barycentric coordinates for v in omega.
+
+
+end
+
